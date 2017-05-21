f9453e69877f5469a3fe4db168ed00519ce83b56
Coding <frankgucci@mac.com>
2017-05-21 13:11:18 -0500

    updates ro Firebase Auth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,11 +20,7 @@ $('.authenticate-button').on("click", function (event) {
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
+          firebase.auth.GithubAuthProvider.PROVIDER_ID
         ],
       };
       console.log(uiConfig);
