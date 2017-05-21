19afa3210092836e319deeb836c9c8b44b366dc0
Coding <frankgucci@mac.com>
2017-05-21 14:01:06 -0500

    updates ro Firebase Auth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,15 +17,16 @@
 $('.authenticate-button').on("click", function (event) {
 	event.preventDefault();
 	var uiConfig = {
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID
-        ],
-      };
-      console.log(uiConfig);
-      // Initialize the FirebaseUI Widget using Firebase.
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig);
+    signInSuccessUrl: 'index2.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID
+      ],
+    };
+    console.log(uiConfig);
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
 });
