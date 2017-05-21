59fc49eded6b710d1889ad1ef280581ce9d53323
Coding <frankgucci@mac.com>
2017-05-21 09:35:28 -0500

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,22 +17,16 @@
 $('.authenticate-button').on("click", function (event) {
 	event.preventDefault();
 	alert("clicked");
-	var provider = new firebase.auth.GithubAuthProvider();
-  console.log(provider);
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+	var uiConfig = {
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        ],
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
 });
