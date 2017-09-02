cfdbfeb5625624134b5f10b24afaf31072f4e421
brianpatrickhummel <frankgucci@mac.com>
2017-09-02 13:18:54 -0500

    after changing git remote name, had to adjust all links to the gitpages deployment site within app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,31 @@
-
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDM8wo0QlZTF5UC1_X1bOKbXlJQLutzlE8",
-    authDomain: "trainscheduler-1d314.firebaseapp.com",
-    databaseURL: "https://trainscheduler-1d314.firebaseio.com",
-    projectId: "trainscheduler-1d314",
-    storageBucket: "trainscheduler-1d314.appspot.com",
-    messagingSenderId: "383844800617"
+  apiKey: "AIzaSyDM8wo0QlZTF5UC1_X1bOKbXlJQLutzlE8",
+  authDomain: "trainscheduler-1d314.firebaseapp.com",
+  databaseURL: "https://trainscheduler-1d314.firebaseio.com",
+  projectId: "trainscheduler-1d314",
+  storageBucket: "trainscheduler-1d314.appspot.com",
+  messagingSenderId: "383844800617"
 };
 firebase.initializeApp(config);
 
-  //Github provider object for authentication
+//Github provider object for authentication
 
 var uiConfig = {
-  signInSuccessUrl: 'https://brianpatrickhummel.github.io/week7-trainscheduler/index2.html',
+  signInSuccessUrl:
+    "https://brianpatrickhummel.github.io/trainscheduler/index2.html",
   signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
-      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      // firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-  };
-  console.log(uiConfig);
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
-
-
-
-
-
-
-
-   
-
-
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID
+    // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ]
+};
+console.log(uiConfig);
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start("#firebaseui-auth-container", uiConfig);
--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,48 +1,61 @@
-function logUserOut(){
-      firebase.auth().signOut().then(function() {
+function logUserOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(
+      function() {
         console.log("Sign-out successful.");
-      }, function(error) {
+      },
+      function(error) {
         console.log("An error happened.");
-      });
-    }
-
+      }
+    );
+}
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDM8wo0QlZTF5UC1_X1bOKbXlJQLutzlE8",
-    authDomain: "trainscheduler-1d314.firebaseapp.com",
-    databaseURL: "https://trainscheduler-1d314.firebaseio.com",
-    projectId: "trainscheduler-1d314",
-    storageBucket: "trainscheduler-1d314.appspot.com",
-    messagingSenderId: "383844800617"
+  apiKey: "AIzaSyDM8wo0QlZTF5UC1_X1bOKbXlJQLutzlE8",
+  authDomain: "trainscheduler-1d314.firebaseapp.com",
+  databaseURL: "https://trainscheduler-1d314.firebaseio.com",
+  projectId: "trainscheduler-1d314",
+  storageBucket: "trainscheduler-1d314.appspot.com",
+  messagingSenderId: "383844800617"
 };
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$('.submit-button').on("click", function(event) {
+$(".submit-button").on("click", function(event) {
   event.preventDefault();
-  var trainName = $('#trainName').val().trim();
-  var destination = $('#destination').val().trim();
-  var firstTrain = moment(($('#firstTrain').val()), "HH:mm").format("X");
-  var frequency = parseInt($('#frequency').val().trim());
+  var trainName = $("#trainName")
+    .val()
+    .trim();
+  var destination = $("#destination")
+    .val()
+    .trim();
+  var firstTrain = moment($("#firstTrain").val(), "HH:mm").format("X");
+  var frequency = parseInt(
+    $("#frequency")
+      .val()
+      .trim()
+  );
 
-  database.ref().push( {
+  database.ref().push({
     trainName: trainName,
     destination: destination,
     firstTrain: firstTrain,
     frequency: frequency
   });
 
-  $('#trainName').val("");
-  $('#destination').val("");
-  $('#firstTrain').val("");
-  $('#frequency').val("");
+  $("#trainName").val("");
+  $("#destination").val("");
+  $("#firstTrain").val("");
+  $("#frequency").val("");
 });
 
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-  var trainName = (childSnapshot.val().trainName).toUpperCase();
-  var destination = (childSnapshot.val().destination).toUpperCase();
+  var trainName = childSnapshot.val().trainName.toUpperCase();
+  var destination = childSnapshot.val().destination.toUpperCase();
   var firstTrain = moment.unix(childSnapshot.val().firstTrain).format("HH:mm");
   var frequency = childSnapshot.val().frequency;
 
@@ -55,7 +68,7 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   // Time apart (remainder)
   var tRemainder = diffTime % frequency;
- 
+
   // Minute Until Train
   var minutesAway = frequency - tRemainder;
   console.log("MINUTES TILL TRAIN: " + minutesAway);
@@ -64,16 +77,23 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   var b = moment().add(minutesAway, "minutes");
   var nextArrival = moment(b).format("HH:mm");
 
-$(".trainSchedule").prepend("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
-
+  $(".trainSchedule").prepend(
+    "<tr><td>" +
+      trainName +
+      "</td><td>" +
+      destination +
+      "</td><td>" +
+      frequency +
+      "</td><td>" +
+      nextArrival +
+      "</td><td>" +
+      minutesAway +
+      "</td></tr>"
+  );
 });
-
 
 $(".authenticate-button").on("click", function() {
   localStorage.clear();
   sessionStorage.clear();
   logUserOut();
 });
-
-
-
